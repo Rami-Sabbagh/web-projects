4e9352ed14566e4eca1959cbf6aec13f62f6eaac
Rami Sabbagh <ramilego4game@gmail.com>
2020-10-31 01:07:20 +0200

    Cleanup Game.js

--- snake/snake-game/Game.js
+++ snake/snake-game/Game.js
@@ -3,93 +3,111 @@ import Snake from './classes/Snake.js';
 import Fruit from './classes/Fruit.js';
 
 export default class Game {
-    _timer = 0;
-    _timerHandle = undefined;
 
-    constructor(width = 30, height = 20, initialFruits = 5) {
-        this.tileMap = new TileMap(width, height);
+    //==-- Public Fields --==//
 
-        this.snake = new Snake(this.tileMap, Math.floor(width / 2) + 2, Math.floor(height / 2));
-        this.snake.onDeath = this.onDeath.bind(this);
+    startingSnakeLength = undefined;
 
-        for (let i = 0; i < initialFruits; i++) {
-            this.spawnNewFruit();
+    //==-- Private Fields --==//
+
+    _tileMap = undefined;
+    _fruitsCount = undefined;
+    _snake = undefined;
+    _fruitConsumtionHandler = undefined;
+
+    //==-- Constructor --==//
+
+    //- width (Number): The width of the game's map in tiles, defaults to (30).
+    //- height (Number): The height of the game's map in tiles, defaults to (20).
+    //- fruitsCount (Number): The number of fruits in the game.
+    //- startingSnakeLength (Number): The starting length of the snake.
+    constructor(width, height, fruitsCount = 5, startingSnakeLength = 5) {
+        this._tileMap = new TileMap(width, height);
+        this._fruitsCount = fruitsCount;
+        this.startingSnakeLength = startingSnakeLength;
+
+        this._snake = new Snake(this._tileMap);
+        this._snake.onDeath = () => this.onDeath();
+        this._fruitConsumtionHandler = () => {
+            this.spawnFruit();
+            this.onFruitConsumed();
+        };
+    }
+
+    //==-- Getters and Setters --==//
+
+    get element() { return this._tileMap.element; }
+    get tileMap() { return this._tileMap; }
+    get snake() { return this._snake; }
+
+    //==-- Callbacks --==//
+
+    //Called when the snake dies.
+    onDeath() { }
+
+    //Called when the fruit is consumed.
+    onFruitConsumed() { }
+
+    //==-- Methods --==//
+
+    //Setups and starts/restarts the game.
+    start() {
+        this.setup();
+        this.resume();
+    }
+
+    //Pauses the game.
+    pause() {
+        this._snake.pause();
+    }
+
+    //Starts/Resumes the game.
+    resume() {
+        if (!this._snake.spawned) { throw new Error("The snake is not spawned for the game to resume, call .setup() first before resuming the game."); }
+        this._snake.resume();
+    }
+
+    //Setups the game's entities.
+    setup() {
+        if (this._snake.spawned) { this.reset(); }
+        this.spawnSnake();
+        this.spawnStartingFruits();
+    }
+
+    //Resets the game's state, unspawning all the entities.
+    reset() {
+        this._snake.destroy();
+        this._tileMap.clearEntities();
+    }
+
+    //Spawns/Respawns the game's snake.
+    spawnSnake() {
+        this._snake.respawn(
+            Math.floor((this._tileMap.width - this.startingSnakeLength)/2),
+            Math.floor(this._tileMap.height/2),
+            this.startingSnakeLength
+        );
+    }
+
+    //Spawns the starting fruits.
+    spawnStartingFruits() {
+        for (let i = 0; i < this._fruitsCount; i++) {
+            this.spawnFruit();
         }
-
-        this._timerHandle = setInterval(() => {
-            this._timer++;
-            this.updateTimerDisplay();
-        }, 1000);
-        this.updateScoreDisplay();
-
-        document.querySelector(".game-over .restart").addEventListener("click", ev => {
-            document.querySelector(".game-over").classList.remove("active");
-            this.snake.destroy();
-            this.tileMap.clearEntities();
-            this.snake.respawn(Math.floor(width / 2) + 2, Math.floor(height / 2));
-
-            for (let i = 0; i < initialFruits; i++) {
-                this.spawnNewFruit();
-            }
-
-            this.snake.resume();
-            //TODO: Fix timer no longer working after restart
-        });
     }
 
-    get element() {
-        return this.tileMap.element;
-    }
-
-    spawnNewFruit() {
+    //Spawns a fruit at a random empty location.
+    spawnFruit() {
         let x = Math.floor(Math.random() * this.tileMap.width);
         let y = Math.floor(Math.random() * this.tileMap.height);
 
         if (this.tileMap.getEntity(x, y)) {
-            this.spawnNewFruit();
+            this.spawnFruit();
             return;
         }
 
         let fruit = new Fruit();
-        fruit.onConsumtion = this.onFruitConsumed.bind(this);
+        fruit.onConsumtion = this._fruitConsumtionHandler;
         this.tileMap.addEntity(fruit, x, y);
-    }
-
-    formatTimer() {
-        let seconds = (this._timer % 60).toString();
-        let minutes = Math.floor(this._timer / 60).toString();
-
-        if (seconds.length === 1) { seconds = "0" + seconds; }
-        if (minutes.length === 1) { minutes = "0" + minutes; }
-
-        return minutes + ":" + seconds;
-    }
-
-    updateScoreDisplay() {
-        document.querySelector(".score-value").innerHTML = this.snake.length.toString();
-    }
-
-    updateTimerDisplay() {
-        document.querySelector(".timer-value").innerHTML = this.formatTimer();
-    }
-
-    onFruitConsumed() {
-        this.spawnNewFruit();
-        this.updateScoreDisplay();
-    }
-
-    onDeath() {
-        //Stop the game timer
-        if (this._timerHandle !== undefined) {
-            clearInterval(this._timerHandle);
-            this._timerHandle = undefined;
-        }
-
-        let overlay = document.querySelector(".game-over");
-        let scoreDisplay = document.querySelector(".game-over .score");
-
-        scoreDisplay.innerHTML = `<b>Score:</b> ${this.snake.length.toString()} - <b>Time:</b> ${this.formatTimer()}`;
-
-        overlay.classList.add("active");
     }
 }
