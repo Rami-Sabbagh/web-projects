a7491791640e7b5fcbaae375d68fdab685e12680
Rami Sabbagh <ramilego4game@gmail.com>
2020-10-30 23:41:05 +0200

    Cleanup Fruit.js

--- snake/snake-game/classes/Fruit.js
+++ snake/snake-game/classes/Fruit.js
@@ -1,7 +1,24 @@
 import Entity from './Entity.js';
 
+//Represents a fruit in the game.
 export default class Fruit extends Entity {
+
+    //==-- Constructor --==//
+
     constructor() {
         super("fruit");
+    }
+
+    //==-- Callbacks --==//
+
+    onConsumtion() { }
+
+    //==-- Methods --==//
+
+    //--(overrides Entity)--//
+    consumed(snake) {
+        this.onConsumtion();
+        snake.grow();
+        return false;
     }
 }
