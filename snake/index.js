ed146e1f7b4b05905b268d3dbb113303d39d1c87
Rami Sabbagh <ramilego4game@gmail.com>
2020-10-30 23:42:55 +0200

    Link the keyboard controls handler

--- snake/index.js
+++ snake/index.js
@@ -1,8 +1,10 @@
 import Game from './snake-game/Game.js';
+import StandardKeyboardControls from './snake-game/StandardKeyboardControls.js';
 import Gamepad2Keyboard from './snake-game/Gamepad2Keyboard.js';
 
 let game = new Game(30,20);
 let root = document.querySelector(".game-container");
 root.insertBefore(game.element, root.firstChild)
 
+new StandardKeyboardControls(game.snake);
 new Gamepad2Keyboard();
