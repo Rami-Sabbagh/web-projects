14f5d6158aac263e92ba87d18cd3e033b074a406
Rami Sabbagh <ramilego4game@gmail.com>
2020-10-30 23:40:55 +0200

    Cleanup Entity.js

--- snake/snake-game/classes/Entity.js
+++ snake/snake-game/classes/Entity.js
@@ -1,9 +1,23 @@
+//An entity is a game object which occupies a tile.
+//It's expected to extend this class, and call the super constructor with a specific class name.
 export default class Entity {
+
+    //==-- Contructor --==//
+
+    //- className (String): The name of the CSS class to apple on the tile element which the entity occupies.
     constructor(className) {
         this._className = className;
     }
 
+    //==-- Getters and Setters --==//
+
     get className() {
         return this._className;
     }
+
+    //==-- Methods --==//
+
+    //Consume the entity by a snake.
+    //return: rejectConsumtion: (true) to reject the consumtion of this entity, causing the death of the snake.
+    consumed(snake) { return false; }
 }
