92ed5cfda40d2f503d3a13c2d5206a1b11b7622d
Rami Sabbagh <ramilego4game@gmail.com>
2020-10-30 23:42:15 +0200

    Cleanup SnakePiece.js

--- snake/snake-game/classes/SnakePiece.js
+++ snake/snake-game/classes/SnakePiece.js
@@ -1,7 +1,18 @@
 import Entity from './Entity.js';
 
+//Represents a piece of the snake in the game.
 export default class SnakePiece extends Entity {
+
+    //==-- Constructor --==//
+
     constructor() {
         super("snake-piece");
+    }
+
+    //==-- Methods --==//
+
+    //--(overrides Entity)--//
+    consumed(snake) {
+        return true; //reject the consumtion.
     }
 }
