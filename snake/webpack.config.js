b3559cae191c0dff5b1254f8ae711e8ca141556b
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-27 10:49:11 +0200

    configure webpack

---
+++ snake/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mode: 'production',
+    entry: './index.js',
+    output: {
+        filename: 'index.bundle.js',
+        path: __dirname
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            }
+        ]
+    }
+}
